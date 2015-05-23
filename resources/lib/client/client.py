b8f66f64f89e926cceb97ca7ddc0124a24c81e7d
Jani Mikkonen <jani.mikkonen@gmail.com>
2015-05-24 00:18:19 +0300

    client can now login!

--- resources/lib/client/client.py
+++ resources/lib/client/client.py
@@ -6,14 +6,16 @@ from .epg import Epg
 from .vod import Vod
 
 
-import requests
-
+import urllib2
+import json
 
 class Client:
 
   apiEndPoint = ''
   loginService = ''
-  versionString = 'libStreamingClient/0.1.5 20150313 (friday the 13th edition)'
+  clientName = None
+  clientVersion = None
+  versionString = None
   apiVersion = '1'
 
   _user = {}
@@ -23,20 +25,61 @@ class Client:
   _cacheServers = {}
   _settings = {}
 
-  def __init__(self, host):
+  _plugin = None
+
+  _sessionId = None
+  _xlibversion = 'libStreamingClient/0.1.5 20150313 (friday the 13th edition)'
+
+  def __init__(self, host, plugin):
+    self._plugin = plugin
+    self._useProxy = plugin.get_setting('use_proxy',bool)
     self.apiEndPoint = 'http://api.%s' % host
     self.loginService = 'http://login.%s' % host
+    self.clientName = plugin.addon.getAddonInfo('id')
+    # getAddonInfo version doesnt seem to work  ? returns "Unavailable"
+    # self.clientVersion = plugin.addon.getAddonInfo('version')
+    self.clientVersion = "0.0.1"
+    self.versionString = "%s/%s" % (self.clientName, self.clientVersion )
+    self.User = User(self, plugin)
     pass
 
+  def setSessionId(self, id):
+    print "GOT SESSION!!!!!!!!!", id
+    self._sessionId = id
 
-  def request(self, apiMethod, data, path):
-    reqMethod = 'GET'
-    if data:
-    	reqMethod = 'POST'
-
-
+  def request(self, apiMethod, data=None, path=None):
     uri = path
     if not path:
     	uri = "%s/%s/%s" % (self.apiEndPoint, self.apiVersion, apiMethod)
 
-    print uri
+    conn = None
+    if data != None:
+      payload = json.dumps(data)
+      req = urllib2.Request(uri, payload)
+      contentLength = len(payload)
+      req.add_header('Content-Length', contentLength)
+      req.add_header('Content-Type', 'application/json')
+    else:
+      req = urllib2.Request(uri)
+
+    req.add_header('User-Agent', self.versionString)
+    # req.add_header('X-LIBVERSION', self._xlibversion)
+    if self._sessionId != None:
+      req.add_header('X-SESSION', self._sessionId)
+
+    if self._useProxy:
+      proxies = {
+        'http': 'http://' + plugin.get_setting('proxy_host',unicode) + ":" + plugin.get_setting('proxy_port', unicode),
+        'https': 'http://' + plugin.get_setting('proxy_host', unicode) + ":" + plugin.get_setting('proxy_port', unicode),
+      }
+      opener = urllib2.build_opener(urllib2.ProxyHandler(proxies, debuglevel=1))
+      urllib2.install_opener(opener)
+      conn = opener.open(req)
+    else:
+      opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
+      urllib2.install_opener(opener)
+      conn = opener.open(req)
+
+    resp = conn.read()
+    conn.close()
+    return resp
--- resources/lib/client/user.py
+++ resources/lib/client/user.py
@@ -1,6 +1,23 @@
 __author__ = 'rasjani'
 
+import json
+
 
 class User:
-  def __init__(self):
+  def __init__(self, client, plugin):
+    self._client = client
+    self._plugin = plugin
+
+    self.login()
     pass
+
+  def login(self):
+    email = self._plugin.get_setting('email', unicode)
+    password = self._plugin.get_setting('password', unicode);
+
+    response = json.loads(self._client.request('user/login',  {'password':password,'email':email }))
+
+    if response['code'] == 'login_ok':
+      self._client.setSessionId(response['payload'])
+    else:
+      self._plugin.log.debug("Missing error handling")
